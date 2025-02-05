20aa9b1bf2076c88300ab6b17d82be43794e2055
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-02-05 08:45:47 +0800

    feat: Add first_program.py with examples of string declarations and prints

---
+++ C1 - Basic Syntax/first_program.py
@@ -0,0 +1,7 @@
+single_quote = 'This is a single quote string.'
+double_quote = "This is a double quote string."
+triple_quote = '''This is a triple quote string.'''
+
+print(single_quote)
+print(double_quote)
+print(triple_quote)
