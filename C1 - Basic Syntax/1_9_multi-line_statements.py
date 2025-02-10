d46fe4cd6bbb63475bbc0aeace8a62a957b17e91
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-02-10 23:11:07 +0800

    fix: Update .gitignore to include Module directory and adjust multi-line statements formatting in 1_9_multi-line_statements.py

--- C1 - Basic Syntax/1_9_multi-line_statements.py
+++ C1 - Basic Syntax/1_9_multi-line_statements.py
@@ -2,8 +2,14 @@ numbers = [1, 2, 3, 4, 5,
            6, 7, 8, 9, 10]
 
 total = (1 + 2 + 3 +
-            4 + 5 + 6 +
-            7 + 8 + 9 + 10)
+         4 + 5 + 6 +
+         7 + 8 + 9 +
+         10)
 
-colors = {'red': 255, 'green': 255,
-            'blue': 255}
+colors = {
+    'red': 255,
+    'green': 255,
+    'blue': 255
+}
+
+print(colors)
