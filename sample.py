79612027649cae7d50012a0dcded594b4ecf1562
Jarrian Gojar <jarrianvincegojar@gmail.com>
2025-03-11 08:22:41 +0800

    feat: Lesson on Functions

---
+++ sample.py
@@ -0,0 +1,22 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+age = int(input("What is your age? "))
+
+print(f"Hello, {first_name} {last_name}! You are {age} years old.")
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+sum1 = num1 + num2
+difference1 = num1 - num2
+product1 = num1 * num2
+quotient1 = num1 / num2
+
+print("Sum: ", sum1)
+print("Difference: ", difference1)
+print("Product: ", product1)
+print("Quotient: ", quotient1)
+
+color = input("What is your favorite color? ")
+
+print(color, " is a beautiful color!")
---
+++ sample2.py
@@ -0,0 +1,21 @@
+# Exercise 1
+# Doe, John
+num = 0
+
+for i in range(1, 11):
+    num += i
+
+# Exercise 2
+# Doe, John
+print(num)
+
+# Exercise 3
+# Doe, John
+num = 0
+
+while num < 10:
+    num += 1
+    if num % 2 == 0:
+        print(num, "is even.")
+    else:
+        print(num, "is odd.")
